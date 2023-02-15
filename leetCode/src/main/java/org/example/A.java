28f0a70d3327d38948d32a8e7832713a431a6772
Andrey Fedorov <dron81998@yandex.ru>
2023-02-15 09:46:35 +0300

    67

--- leetCode/src/main/java/org/example/A.java
+++ leetCode/src/main/java/org/example/A.java
@@ -1,6 +1,39 @@
 package org.example;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class A {
+    public List<Integer> addToArrayForm_989(int[] num, int k) {
+        LinkedList<Integer> arrayList = new LinkedList<>();
+        int i = num.length-1;
+        boolean key1 = true,key2 = true;
+        int a=0,b=0,s;
+        int next = 0;
+        while (true){
+            if (i>=0){
+                a = num[i];
+                i--;
+            }
+            else key1 = false;
+            if (k>0){
+                b = k % 10;
+                k/=10;
+            }
+            else key2 = false;
+            if (!key1 && !key2 && next == 0) break;
+            s = a+b+next;
+            next = 0;
+            if (s>=10){
+                next=1;
+                s = s- 10;
+            }
+            arrayList.addFirst(s);
+            a=0;b=0;
+        }
+        return arrayList;
+    }
+
     public static String addBinary_67(String a, String b) {
         char[] max;
         char[] min;
