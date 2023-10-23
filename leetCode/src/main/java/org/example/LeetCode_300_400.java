ef953d90a2e9bf6463023e381b90aed7a16e8f9a
Skell1 <46198113+Skell1@users.noreply.github.com>
2023-10-23 16:03:03 +0300

    342

--- leetCode/src/main/java/org/example/LeetCode_300_400.java
+++ leetCode/src/main/java/org/example/LeetCode_300_400.java
@@ -2,6 +2,18 @@ package org.example;
 
 public class LeetCode_300_400 {
     public static void main(String[] args) {
+        System.out.println(isPowerOfFour(-16));
+    }
 
+    public static boolean isPowerOfFour(int n) {//342
+        if (n< 0 ) return false;
+       while (n >= 4 || n <= -4) {
+            if (n % 4 != 0) return false;
+            n /= 4;
+        }
+        if (n == 1) {
+            return true;
+        }
+        else return false;
     }
 }
