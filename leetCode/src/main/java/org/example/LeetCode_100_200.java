1f4e588e17efa7f9a13f475a28f85d54c5b8a952
a.fedorov <a.fedorov@cfa.digital>
2023-12-07 16:03:41 +0300

    189

--- leetCode/src/main/java/org/example/LeetCode_100_200.java
+++ leetCode/src/main/java/org/example/LeetCode_100_200.java
@@ -1,7 +1,23 @@
 package org.example;
 
+
 public class LeetCode_100_200 {
     public static void main(String[] args) {
+        int[] a = new  int[] {1,2,3,4,5,6,7};
+        int[] b = new  int[] {-1,-100,3,99};
+        rotate(a,3);
 
+        rotate(b,2);
     }
+
+    public static void rotate(int[] nums, int k) { // 189
+        final int size = nums.length;
+        if (k > size) k = k % size;
+        int[] a = new int[size-k];
+        int[] b = new int[k];
+        System.arraycopy(nums, 0, a, 0, size-k);
+        System.arraycopy(nums, size-k, b, 0, k);
+        System.arraycopy(b, 0, nums, 0,k);
+        System.arraycopy(a, 0, nums, k, size-k);
+   }
 }
