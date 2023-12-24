954b715eb1af78090cfa92e9a3ecc1ac9049066b
a.fedorov <a.fedorov@cfa.digital>
2023-12-24 22:30:48 +0300

    1758

--- leetCode/src/main/java/org/example/LeetCode_1700_1800.java
+++ leetCode/src/main/java/org/example/LeetCode_1700_1800.java
@@ -2,7 +2,8 @@ package org.example;
 
 public class LeetCode_1700_1800 {
     public static void main(String[] args) {
-        System.out.println(totalMoney(10));
+
+        System.out.println(minOperations("1111"));
     }
 
     public static int totalMoney(int n) { //1716
@@ -15,5 +16,22 @@ public class LeetCode_1700_1800 {
             week++;
         }
         return k;
+    }
+
+    public static int minOperations(String s) { //1758
+        int k0 = 0, k1 = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i % 2 == 0){
+                if (s.charAt(i) == '1')
+                    k0++;
+                else k1++;
+            }
+            else {
+                if (s.charAt(i) == '1')
+                    k1++;
+                else k0++;
+            }
+        }
+        return Math.min(k0, k1);
     }
 }
