4a4901c0298eaeee015e496f732b7fb51157b744
a.fedorov <a.fedorov@cfa.digital>
2024-01-29 13:19:13 +0300

    232

--- leetCode/src/main/java/org/example/LeetCode_200_300.java
+++ leetCode/src/main/java/org/example/LeetCode_200_300.java
@@ -1,7 +1,54 @@
 package org.example;
 
+import java.util.Objects;
+import java.util.Queue;
+import java.util.Stack;
+
 public class LeetCode_200_300 {
     public static void main(String[] args) {
+        MyQueue myQueue = new MyQueue();
+        myQueue.push(1);
+        myQueue.push(2);
+        int a = myQueue.peek();
+        int b = myQueue.pop();
+        myQueue.empty();
 
+        System.out.println(myQueue.empty());
+    }
+
+    public static class MyQueue {  // 232
+
+        private int front;
+        private Stack<Integer> s1 = new Stack<>();
+        private Stack<Integer> s2 = new Stack<>();
+
+
+        public void push(int x) {
+            if (s1.isEmpty()){
+                front = x;
+            }
+            s1.push(x);
+        }
+
+        public int pop() {
+            if(s2.isEmpty()){
+                while(!s1.isEmpty()){
+                    s2.push(s1.pop());
+                }
+            }
+            return s2.pop();
+        }
+
+        public int peek() {
+            if (!s2.isEmpty()) {
+                return s2.peek();
+            }
+
+            return front;
+        }
+
+        public boolean empty() {
+            return (s1.isEmpty() && s2.isEmpty());
+        }
     }
 }
