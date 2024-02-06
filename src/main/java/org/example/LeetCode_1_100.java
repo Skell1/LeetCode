c4593b1b2c6b43c28c6313016c48356b385dcef3
a.fedorov <a.fedorov@cfa.digital>
2024-02-06 19:02:59 +0300

    49

--- src/main/java/org/example/LeetCode_1_100.java
+++ src/main/java/org/example/LeetCode_1_100.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.*;
+
 public class LeetCode_1_100 {
     public static void main(String[] args) {
 //        System.out.println(mySqrt(4));
@@ -8,6 +10,39 @@ public class LeetCode_1_100 {
 
 
     }
+
+    public static List<List<String>> groupAnagrams(String[] strs) { //49
+        Map<String, List<String>> map = new HashMap<>();
+//        StringBuilder builder = new StringBuilder();
+        String buld;
+        //  ArrayList<String> list1 = new ArrayList<>();
+        for (int i = 0; i < strs.length; i++) {
+            char[] chars = strs[i].toCharArray();
+            Arrays.sort(chars);
+//            for (char ch : chars) {
+//                builder.append(ch).append("-");
+//            }
+//            buld = builder.toString();
+            buld = new String(chars);
+            map.computeIfAbsent(buld, k -> new ArrayList<>()).add(strs[i]);
+//            if (map.containsKey(buld)) {
+//                map.get(buld).add(strs[i]);
+//            }
+//            else {
+//                list1 = new ArrayList<>();
+//                list1.add(strs[i]);
+//                map.put(builder.toString(), list1);
+//            }
+//            builder.setLength(0);
+        }
+//        List<List<String>> result = new ArrayList<>();
+//        for (List<String> list : map.values()) {
+//            result.add(list);
+//        }
+//        return result;
+        return new ArrayList<List<String>>(map.values());
+    }
+
     public static int mySqrt(int x) { //69
         if(x <= 1) {
             return x;
