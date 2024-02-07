846c6f43578b4e2426e83af116222400d992769c
a.fedorov <a.fedorov@cfa.digital>
2024-02-07 19:18:54 +0300

    451

--- src/main/java/org/example/LeetCode_400_500.java
+++ src/main/java/org/example/LeetCode_400_500.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class LeetCode_400_500 {
     public static void main(String[] args) {
+        frequencySort("tree");
+    }
 
+    public static String frequencySort(String s) { //451
+        Map<Character, StringBuilder> map = new HashMap<>();
+        char ch;
+        for (int i = 0; i < s.length(); i++) {
+            ch = s.charAt(i);
+            map.computeIfAbsent(ch, k -> new StringBuilder()).append(ch);
+        }
+        StringBuilder builder = new StringBuilder();
+        map.values().stream().sorted((s1,s2) -> s2.length() - s1.length()).forEach(builder::append);
+        return builder.toString();
     }
 }
