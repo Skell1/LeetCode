46b074fc72affee1bd3fa71dcb94d7b35c8fd40b
a.fedorov <a.fedorov@cfa.digital>
2024-05-23 10:41:28 +0300

    add

---
+++ src/main/java/org/example/LeetCode_1800_1900.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class LeetCode_1800_1900 {
+    public static void main(String[] args) {
+
+     //   System.out.println(new LeetCode_1800_1900().countStudents(new int[]{1,1,1,0,0,1},new int[]{1,0,0,0,1,1}));
+    }
+
+
+}
