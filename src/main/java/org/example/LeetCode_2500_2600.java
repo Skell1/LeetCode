154e13ce17720917c8e341a2d8d46ab06b6f4f1d
Andrey <@>
2024-07-06 18:12:01 +0300

    2582

--- src/main/java/org/example/LeetCode_2500_2600.java
+++ src/main/java/org/example/LeetCode_2500_2600.java
@@ -2,7 +2,9 @@ package org.example;
 
 public class LeetCode_2500_2600 {
     public static void main(String[] args) {
+        System.out.println(new LeetCode_2500_2600().passThePillow(18,38 )); //5
 
+        System.out.println(new LeetCode_2500_2600().passThePillow(4,5 ));//2
     }
 
     public int getCommon(int[] nums1, int[] nums2) { //2540
@@ -15,5 +17,18 @@ public class LeetCode_2500_2600 {
             else a++;
         }
         return -1;
+    }
+
+    public int passThePillow(int n, int time) { //2582
+        if (time < n) return time+1;
+        int circl = time / (n-1);
+        int count =  time - circl*(n-1);
+
+        if (circl % 2 == 0) {
+            return count;
+        } else {
+            return n - count;
+        }
+
     }
 }
