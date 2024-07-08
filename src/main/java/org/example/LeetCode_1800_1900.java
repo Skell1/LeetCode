6f30ea076c5725d0c78323277e82868e0d9f56d8
Andrey <@>
2024-07-08 23:04:13 +0300

    1823

--- src/main/java/org/example/LeetCode_1800_1900.java
+++ src/main/java/org/example/LeetCode_1800_1900.java
@@ -1,9 +1,28 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class LeetCode_1800_1900 {
     public static void main(String[] args) {
 
-     //   System.out.println(new LeetCode_1800_1900().countStudents(new int[]{1,1,1,0,0,1},new int[]{1,0,0,0,1,1}));
+        System.out.println(new LeetCode_1800_1900().findTheWinner(5,2));
+    }
+
+    public int findTheWinner(int n, int k) { //1823
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 1; i <= n; i++) {
+            list.add(i);
+        }
+        int current = -1;
+        while (list.size() > 1) {
+            current += k;
+            if (current >= list.size()) {
+                current = current%list.size();
+            }
+            list.remove(current);
+        }
+        return list.get(0);
+
     }
 
 
