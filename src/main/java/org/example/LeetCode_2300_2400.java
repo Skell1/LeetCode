10dea91fee0c1269b622e3b7bec6fccd60f0e0e1
Andrey <@>
2024-12-05 10:00:15 +0300

    2337

--- src/main/java/org/example/LeetCode_2300_2400.java
+++ src/main/java/org/example/LeetCode_2300_2400.java
@@ -2,12 +2,40 @@ package org.example;
 
 import org.example.util.TreeNode;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class LeetCode_2300_2400 {
     public static void main(String[] args) {
-        System.out.println(new LeetCode_2300_2400().largestLocal(new int[][]{{9,9,8,1},{5,6,2,6},{8,2,6,4},{6,2,2,2}}));
+        System.out.println(new LeetCode_2300_2400().canChange("_L__R__R_", "L______RR"));
+    }
+
+    public boolean canChange(String start, String target) { //2337
+        Map<Integer, Character> map = new HashMap<>();
+        Map<Integer, Character> map2 = new HashMap<>();
+
+        for (int i = 0; i < start.length(); i++) {
+            if (start.charAt(i) == 'L' || start.charAt(i) == 'R') {
+                map.put(i, start.charAt(i));
+            }
+            if (target.charAt(i) == 'L' || target.charAt(i) == 'R') {
+                map2.put(i, target.charAt(i));
+            }
+        }
+
+        if (map.size() != map2.size()) return false;
+
+        List<Integer> keys = map.keySet().stream().sorted().toList();
+        List<Integer> keys2 = map2.keySet().stream().sorted().toList();
+
+        for (int i = 0; i < keys.size(); i++) {
+            if (map.get(keys.get(i)) != map2.get(keys2.get(i))) return false;
+            if (map.get(keys.get(i)) == 'L' && keys.get(i) < keys2.get(i)) return false;
+            if (map.get(keys.get(i)) == 'R' && keys.get(i) > keys2.get(i)) return false;
+        }
+        return true;
     }
 
     public int[][] largestLocal(int[][] grid) { //2373
