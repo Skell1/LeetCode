3913921c8eac9c1e94c753415fae72c45dd5c0a5
Fedorov Andrey <fedorov.andrey.a@rt.ru>
2025-07-13 14:37:16 +0300

    2410

--- leetCode/src/main/java/org/example/LeetCode_2400_2500.java
+++ leetCode/src/main/java/org/example/LeetCode_2400_2500.java
@@ -1,13 +1,28 @@
 package org.example;
 
-import org.example.util.TreeNode;
-
-import java.util.Map;
-import java.util.Set;
+import java.util.Arrays;
 
 public class LeetCode_2400_2500 {
     public static void main(String[] args) {
 
+    }
+
+    public int matchPlayersAndTrainers(int[] players, int[] trainers) { //2410
+        Arrays.sort(players);
+        Arrays.sort(trainers);
+
+        int k = 0;
+        int count = 0;
+        for (int i = 0; i < players.length; i++) {
+            for (int j = k; j < trainers.length; j++) {
+                if (players[i] <= trainers[j]) {
+                    k = j+1;
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
     }
 
     public int[] applyOperations(int[] nums) { //2460
