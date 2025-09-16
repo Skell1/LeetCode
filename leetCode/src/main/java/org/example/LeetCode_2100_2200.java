6ae04f0dee6c788782128fa0eb846b36bfa1beda
Fedorov Andrey <fedorov.andrey.a@rt.ru>
2025-09-16 17:48:01 +0300

    2197

--- leetCode/src/main/java/org/example/LeetCode_2100_2200.java
+++ leetCode/src/main/java/org/example/LeetCode_2100_2200.java
@@ -1,8 +1,11 @@
 package org.example;
 
+import java.util.List;
+import java.util.Stack;
+
 public class LeetCode_2100_2200 {
     public static void main(String[] args) {
-        System.out.println(firstPalindrome(new String[]{"abc","car","ada","racecar","cool"}));
+        System.out.println(new LeetCode_2100_2200().replaceNonCoprimes(new int[]{31,97561,97561,97561,97561,97561,97561,97561,97561}));
     }
 
     public static String firstPalindrome(String[] words) {//2108
@@ -14,6 +17,49 @@ public class LeetCode_2100_2200 {
             stringBuilder.setLength(0);
         }
         return "";
+    }
+
+    public List<Integer> replaceNonCoprimes(int[] nums) { //2197
+        Stack<Integer> stack = new Stack<>();
+        stack.push(nums[0]);
+        int nod, nok;
+        int peek;
+        for (int i = 1; i < nums.length; i++) {
+
+                peek = stack.peek();
+                nod = findNOD(Math.max(peek, nums[i]), Math.min(peek, nums[i]));
+                if (nod > 1) {
+                    nok = nums[i];
+                    while (true) {
+                        if (stack.isEmpty()) break;
+                        peek = stack.peek();
+                        nod = findNOD(Math.max(peek, nok), Math.min(peek, nok));
+                        if (nod == 1) {
+                            break;
+                        }
+                        nok = peek / nod * nok ;
+                        stack.pop();
+                    }
+                    stack.push(nok);
+                } else {
+                    stack.push(nums[i]);
+                }
+
+        }
+        return stack;
+    }
+
+    private int findNOD(int a, int b) {
+        int temp;
+        while (true){
+            temp = a % b;
+            if (temp == 0) {
+                return b;
+            } else {
+                a = b;
+                b = temp;
+            }
+        }
     }
 
 }
