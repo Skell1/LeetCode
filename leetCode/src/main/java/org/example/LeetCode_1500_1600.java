0f147a96d8960e5cdd5a60ccf3293671190d48f7
Fedorov Andrey <fedorov.andrey.a@rt.ru>
2025-11-16 14:22:34 +0300

    1513

--- leetCode/src/main/java/org/example/LeetCode_1500_1600.java
+++ leetCode/src/main/java/org/example/LeetCode_1500_1600.java
@@ -5,7 +5,28 @@ import java.util.List;
 
 public class LeetCode_1500_1600 {
     public static void main(String[] args) {
-        System.out.println(new LeetCode_1500_1600().threeConsecutiveOdds(new int[]{102,780,919,897,901}));
+        System.out.println(new LeetCode_1500_1600().numSub("0110111"));
+    }
+
+    public int numSub(String s) { //1513
+        long res = 0;
+        int curr = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0') {
+                if (curr == 0) {
+                    continue;
+                } else {
+                    res += (long) (curr + 1) * curr / 2;
+                    curr = 0;
+                }
+            } else {
+                curr++;
+            }
+        }
+        if (curr != 0) {
+            res += (long) (curr + 1) * curr / 2;
+        }
+        return (int) (res % (1000000000+7));
     }
 
     public int numWaterBottles(int numBottles, int numExchange) { //1518
