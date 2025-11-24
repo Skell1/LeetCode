47a711050bc1ff9af118659016cdf8128613f1b2
Fedorov Andrey <fedorov.andrey.a@rt.ru>
2025-11-24 09:27:04 +0300

    1018

--- leetCode/src/main/java/org/example/LeetCode_1000_1100.java
+++ leetCode/src/main/java/org/example/LeetCode_1000_1100.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public class LeetCode_1000_1100 {
     public static void main(String[] args) {
+        System.out.println(new LeetCode_1000_1100().prefixesDivBy5(new int[]{0,1,1,1,1,1}));
     }
 
     public static int maxAncestorDiff(TreeNode root) {
@@ -22,5 +23,19 @@ public class LeetCode_1000_1100 {
         int max1 = nextTreeNode(treeNode.left,lastMax, lastMin);
         int max2 = nextTreeNode(treeNode.right,lastMax, lastMin);
         return max1>max2?max1:max2;
+    }
+
+    public List<Boolean> prefixesDivBy5(int[] nums) { //1018
+        List<Boolean> res = new ArrayList<>();
+        int curr = 0;
+        for (int i = 0; i < nums.length; i++) {
+            curr *= 2;
+            curr+= nums[i];
+            if (curr%5==0) {
+                res.add(true);
+            } else res.add(false);
+            curr %= 5;
+        }
+        return res;
     }
 }
