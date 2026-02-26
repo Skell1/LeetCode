05b6908d0c527a3199cfb733e9568064112a1bc1
Fedorov Andrey <fedorov.andrey.a@rt.ru>
2026-02-26 09:51:48 +0300

    1404

--- leetCode/src/main/java/org/example/LeetCode_1400_1500.java
+++ leetCode/src/main/java/org/example/LeetCode_1400_1500.java
@@ -31,31 +31,6 @@ public class LeetCode_1400_1500 {
         return res;
     }
 
-    public int pseudoPalindromicPaths (TreeNode root) { //1457
-        return pseudoPalindromicPathsUtil(root, new int[9]);
-    }
-
-    public int pseudoPalindromicPathsUtil (TreeNode root, int[] array) {
-        if (root!=null) {
-            array[root.val-1]++;
-            if (root.left == null && root.right == null) {
-                int k = 0;
-                for (int i = 0; i < array.length; i++) {
-                    if (array[i] % 2 != 0) {
-                        k++;
-                        if (k > 1) {
-                            return 0;
-                        }
-                    }
-                }
-                return 1;
-            }
-        }
-        return (root.right != null ? pseudoPalindromicPathsUtil(root.right , Arrays.copyOf(array, array.length)) : 0) + (root.left != null ? pseudoPalindromicPathsUtil(root.left , Arrays.copyOf(array, array.length)) : 0);
-    }
-
-
-
     public static int maxScore(String s) { //1422
         int length = s.length();
         int result = 0;
@@ -93,4 +68,28 @@ public class LeetCode_1400_1500 {
         }
         return true;
     }
+
+    public int pseudoPalindromicPaths (TreeNode root) { //1457
+        return pseudoPalindromicPathsUtil(root, new int[9]);
+    }
+
+    public int pseudoPalindromicPathsUtil (TreeNode root, int[] array) {
+        if (root!=null) {
+            array[root.val-1]++;
+            if (root.left == null && root.right == null) {
+                int k = 0;
+                for (int i = 0; i < array.length; i++) {
+                    if (array[i] % 2 != 0) {
+                        k++;
+                        if (k > 1) {
+                            return 0;
+                        }
+                    }
+                }
+                return 1;
+            }
+        }
+        return (root.right != null ? pseudoPalindromicPathsUtil(root.right , Arrays.copyOf(array, array.length)) : 0) + (root.left != null ? pseudoPalindromicPathsUtil(root.left , Arrays.copyOf(array, array.length)) : 0);
+    }
+
 }
