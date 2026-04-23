4b938f162ae085d745f2b804fdc39b09d9336335
Fedorov Andrey <fedorov.andrey.a@rt.ru>
2026-04-23 21:47:23 +0300

    -

---
+++ leetCode/src/main/java/org/example/LeetCode_2600_2700.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.*;
+
+public class LeetCode_2600_2700 {
+    public static void main(String[] args) {
+        System.out.println(new LeetCode_2600_2700());
+    }
+
+
+}
