317d045200c415e7c8f5f87f493db606b1d253ea
Fedorov Andrey <fedorov.andrey.a@rt.ru>
2026-06-04 12:46:41 +0300

    3751

---
+++ leetCode/src/main/java/org/example/LeetCode_3700_3800.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class LeetCode_3700_3800 {
+    public static void main(String[] args) {
+        System.out.println(new LeetCode_3700_3800().totalWaviness(4848, 4848));
+
+    }
+
+    public int totalWaviness(int num1, int num2) { //3751
+        int count = 0;
+        for (int i = num1; i <= num2; i++) {
+            if (i < 100) {
+                continue;
+            }
+            count += checkInt(i);
+        }
+        return count;
+    }
+
+    private int checkInt(int num) {
+        int res = 0;
+        int first = num % 10;
+        num = num / 10;
+        int second = num % 10;
+        num = num / 10;
+        int third = num % 10;
+        if ((second > first && second > third) || (second < first && second < third)) {
+            res++;
+        }
+        while (num / 10 > 0) {
+            num = num / 10;
+            first = second;
+            second = third;
+            third = num % 10;
+            if ((second > first && second > third) || (second < first && second < third)) {
+                res++;
+            }
+        }
+        return res;
+
+    }
+}
