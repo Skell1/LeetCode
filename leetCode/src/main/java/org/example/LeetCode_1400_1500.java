81d9534086db9de0bfc1c17642511c2a7b679a09
Fedorov Andrey <fedorov.andrey.a@rt.ru>
2026-07-27 09:20:43 +0300

    1464

--- leetCode/src/main/java/org/example/LeetCode_1400_1500.java
+++ leetCode/src/main/java/org/example/LeetCode_1400_1500.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 
 public class LeetCode_1400_1500 {
     public static void main(String[] args) {
-        System.out.println(new LeetCode_1400_1500().numSteps("1101"));
+        System.out.println(new LeetCode_1400_1500().maxProduct(new int[]{ 1,1,1,1}));
     }
 
     public int numSteps(String s) { //1404
@@ -90,6 +90,38 @@ public class LeetCode_1400_1500 {
             }
         }
         return (root.right != null ? pseudoPalindromicPathsUtil(root.right , Arrays.copyOf(array, array.length)) : 0) + (root.left != null ? pseudoPalindromicPathsUtil(root.left , Arrays.copyOf(array, array.length)) : 0);
+    }
+
+    public int maxProduct(int[] nums) { //1464
+        int maxPositive = 0;
+        int maxPositive2 = 0;
+        int maxNegative = 0;
+        int maxNegative2 = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                if (maxNegative2 > nums[i] ) {
+                    if (maxNegative > nums[i] ) {
+                        maxNegative2 = maxNegative;
+                        maxNegative = nums[i];
+                    } else {
+                        maxNegative2 = nums[i];
+                    }
+                }
+            } else {
+                if (maxPositive2 < nums[i] ) {
+                    if (maxPositive < nums[i] ) {
+                        maxPositive2 = maxPositive;
+                        maxPositive = nums[i];
+                    } else {
+                        maxPositive2 = nums[i];
+                    }
+                }
+            }
+        }
+
+        return Math.max((maxPositive2 -1) * (maxPositive -1), (maxNegative2 -1) * (maxNegative -1));
+
     }
 
 }
